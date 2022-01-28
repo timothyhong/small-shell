510d7d89ae60d1a3bde07d8bade5fdc97f7c73c7
Timothy Hong <tshong@protonmail.com>
2022-01-28 10:42:30 -0500

    Command parsing mostly working

--- smallsh.c
+++ smallsh.c
@@ -20,7 +20,7 @@ Program Description: This program is an implementation of a simple shell capable
 #define INPUT_CHAR "<"
 #define OUTPUT_CHAR ">"
 #define COMMENT_CHAR "#"
-#define PID_SYMBOL "$$"
+#define VAR_EXP_CHAR "$$"
 #define MAX_LENGTH 2048
 #define MAX_ARGS 512
 #include <stdio.h>
@@ -33,6 +33,8 @@ Program Description: This program is an implementation of a simple shell capable
 #include <time.h>
 #include <fcntl.h>
 #include <errno.h>
+#include <ctype.h>
+#include "smallsh.h"
 
 /* Struct for commands */
 typedef struct command_t {
@@ -42,21 +44,18 @@ typedef struct command_t {
 	char* inputFile;
 	char* outputFile;
 	int isBackground; // 0 if foreground, 1 if background
-	int isComment; // 0 if not comment, 1 if comment
-	char* comment;
 } command_t;
 
 command_t* createCommand(char* line) {
 	command_t* currCommand = malloc(sizeof(*currCommand));
 	initCommand(currCommand); // set default values
 
-	// copy the line for second parse
+	// copy the line (we will use it for a second parse)
 	char* lineCpy = malloc(strlen(line) * sizeof(char) + 1);
 	strcpy(lineCpy, line);
 
 	char* savePtr = NULL;
 
-	int commentFound = 0;
 	int commandFound = 0;
 	int argsFound = 0;
 	int inputFound = 0;
@@ -67,28 +66,21 @@ command_t* createCommand(char* line) {
 	char* token = strtok_r(line, " ", &savePtr);
 	while (token != NULL) {
 		if (!commandFound) {
-			// check if it's the COMMENT_CHAR (#)
-			if (strcmp(token, COMMENT_CHAR) == 0) {
-				currCommand->isComment = 1;
-				token = strtok_r(NULL, "", &savePtr);
-				currCommand->comment = malloc(sizeof(char) * strlen(token) + 1);
-				strcpy(currCommand->comment, token);
-				commentFound = 1;
-				commandFound = 1;
-			}
-			// otherwise it's treated as a command
-			else {
-				currCommand->command = malloc(sizeof(char) * strlen(token) + 1);
-				strcpy(currCommand->command, token);
-				commandFound = 1;
-			}
+			// perform variable expansion on $$
+			char* pid = malloc(21);
+			sprintf(pid, "%d", getpid());
+			currCommand->command = expandCommand(token, VAR_EXP_CHAR, pid);
+			free(pid);
+			commandFound = 1;
 		}
 		else if (!argsFound) {
-			// find numArgs
+			// get numArgs
 			int numArgs = 0;
 
+			// use a copy of the string to parse through args to get numArgs
 			char* tempPtr = NULL;
-			char* tempToken = strtok_r(lineCpy + strlen(currCommand->command) + 1, " ", &tempPtr); // skip command
+			char* tempToken = strtok_r(lineCpy, " ", &tempPtr);
+			tempToken = strtok_r(NULL, " ", &tempPtr); // advance it once to skip command
 			while (tempToken != NULL && strcmp(tempToken, INPUT_CHAR) != 0 
 				&& strcmp(tempToken, OUTPUT_CHAR) != 0 
 				&& strcmp(tempToken, BACKGROUND_CHAR) != 0) {
@@ -96,7 +88,6 @@ command_t* createCommand(char* line) {
 				tempToken = strtok_r(NULL, " ", &tempPtr);
 			}
 			currCommand->numArgs = numArgs;
-			free(lineCpy);
 
 			// store args
 			currCommand->args = malloc(sizeof(char*) * numArgs); // allocate space for numArgs char ptrs
@@ -112,9 +103,64 @@ command_t* createCommand(char* line) {
 	// NULL token -> exit
 	if (!commandFound) {
 		free(currCommand);
+		free(lineCpy);
 		return NULL;
 	}
+	free(lineCpy);
 	return currCommand;
+}
+
+/*
+ * Function: expandCommand
+ * ----------------------------
+ *   Takes a command string and expands out the VAR_EXP_CHAR into smallsh's PID.
+ *
+ *	 commandStr: the command string to expand
+ *   expStrFrom: the variable string to expand from
+ *   expStrTo: the variable string to expand to
+ *
+ *   returns: a pointer to the expanded string
+ * 
+ *	 notes: must free returned string
+ */
+
+char* expandCommand(char* commandStr, char* expStrFrom, char* expStrTo) {
+	// count variable strings to expand from
+	int i = 0;
+	int newLength = strlen(commandStr);
+	int adjustedLength = strlen(expStrTo) - strlen(expStrFrom);
+	
+	// figure out how much space to allocate for expanded string
+	while (i < strlen(commandStr)) {
+		// if match, allot room for expansion
+		if (strncmp(&commandStr[i], expStrFrom, strlen(expStrFrom)) == 0) {
+			newLength += adjustedLength;
+			i += strlen(expStrFrom);
+		}
+		// doesn't match
+		else {
+			i++;
+		}
+	}
+	i = 0;
+
+	// allocate the space for expanded string
+	char* expandedStr = malloc(sizeof(char) * newLength + 1);
+	memset(expandedStr, '\0', newLength + 1);
+
+	// now copy the expanded string
+	while (i < strlen(commandStr)) {
+		if (strncmp(&commandStr[i], expStrFrom, strlen(expStrFrom)) == 0) {
+			strncat(expandedStr, expStrTo, strlen(expStrTo));
+			i += strlen(expStrFrom);
+		}
+		// doesn't match
+		else {
+			strncat(expandedStr, &commandStr[i], 1);
+			i++;
+		}
+	}
+	return expandedStr;
 }
 
 /*
@@ -134,11 +180,33 @@ char* getCommand(char** bufPtr, size_t* size) {
 	printf(PROMPT_CHAR);
 	nread = getline(bufPtr, size, stdin);
 	// input validation (in case user just presses enter)
-	while (nread == 1) {
+	// also check if empty space or if user starts with COMMENT_CHAR (#)
+	while (nread == 1 || strncmp(*bufPtr, COMMENT_CHAR, 1) == 0 || isEmptyString(*bufPtr)) {
 		printf(PROMPT_CHAR);
 		nread = getline(bufPtr, size, stdin);
 	}
+	// strip new line
+	if ((*bufPtr)[nread - 1] == '\n') {
+		(*bufPtr)[nread - 1] = '\0';
+	}
 	return *bufPtr;
+}
+/*
+ * Function: isEmptyString
+ * ----------------------------
+ *   Checks if a given string is all empty space.
+ *
+ *   s: a pointer to the string to check
+ * 
+ *	 returns: 0 if not empty; 1 if empty
+ */
+int isEmptyString(char* s) {
+	for (int i = 0; i < strlen(s); i++) {
+		if (!isspace(*s)) {
+			return 0;
+		}
+	}
+	return 1;
 }
 
 /*
@@ -149,18 +217,6 @@ char* getCommand(char** bufPtr, size_t* size) {
  *   command: a pointer to the command struct to print out
  */
 void printCommand(command_t* command) {
-	if (command->isComment) {
-		printf("Comment: True\n");
-		if (command->comment) {
-			printf("Comment Text: %s\n", command->comment);
-		}
-		else {
-			printf("No comment text.\n");
-		}
-	}
-	else {
-		printf("Comment: False\n");
-	}
 
 	if (command->isBackground) {
 		printf("Background: True\n");
@@ -211,8 +267,6 @@ void initCommand(command_t* command) {
 	command->inputFile = NULL;
 	command->outputFile = NULL;
 	command->isBackground = 0; // 0 if foreground, 1 if background
-	command->isComment = 0; // 0 if not comment, 1 if comment
-	command->comment = NULL;
 }
 
 /*
@@ -223,11 +277,11 @@ void initCommand(command_t* command) {
  *   command: a pointer to the command structure to destroy
  */
 void destroyCommand(command_t* command) {
+	if (command->command) {
+		free(command->command);
+	}
 	if (command->args) {
 		free(command->args);
-	}
-	if (command->comment) {
-		free(command->comment);
 	}
 	if (command->inputFile) {
 		free(command->inputFile);
@@ -255,6 +309,10 @@ int startShell(void) {
 		currLine = getCommand(&buffPtr, &size);
 		command_t* currCommand = createCommand(currLine);
 		printCommand(currCommand);
+		if (strcmp(currCommand->command, "exit") == 0) {
+			exit = 1;
+		}
+		free(buffPtr);
 		destroyCommand(currCommand);
 	}
 	return 0;
--- smallsh.h
+++ smallsh.h
@@ -1,4 +1,14 @@
 #pragma once
+#include <sys/types.h>
 
 
-// typedef struct command_t command_t;
+typedef struct command_t command_t;
+command_t* createCommand(char* line);
+char* getCommand(char** bufPtr, size_t* size);
+void printCommand(command_t* command);
+void initCommand(command_t* command);
+void destroyCommand(command_t* command);
+int startShell(void);
+int main(int argc, char* argv[]);
+int isEmptyString(char* s);
+char* expandCommand(char* commandStr, char* expStrFrom, char* expStrTo);
